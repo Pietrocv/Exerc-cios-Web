c0e156751e0e829c0b4768edebbe0f7655297c24
Pietro Visentin <Pietrocvisentin@gmail.com>
2022-05-23 15:49:57 -0300

    Diversas atualizações

--- Exercícios Web/Exercício01/index.js
+++
@@ -1,3 +0,0 @@
-const init = () =>{
-    window.location.assign('../Exercício02/bebe.html')
-}
--- Exercícios Web/Tela-Escolha/escolha.js
+++ Exercícios Web/Tela-Escolha/escolha.js
@@ -1,6 +1,13 @@
+const dias_vida = () =>{
+    window.location.assign('../Tela-CalculadoraDias/calculadoradias.html')
+}
 const corrida = () =>{
     window.location.assign('../Tela-Corrida/corrida.html')
 }
 const peso_ideal = () =>{
     window.location.assign('../Tela-PesoIdeal/pesoideal.html')
 }
+const tamanho_bebe = () =>{
+    window.location.assign('../Tela-TamanhoBebe/bebe.html')
+}
+
